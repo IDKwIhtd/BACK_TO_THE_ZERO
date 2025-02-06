57263a5de8a9cf353716c8baabd178dce56e66b4
SUBIN <giyom2ng@gmail.com>
2025-02-06 14:11:53 +0900

    회원 기능

---
+++ accounts/forms.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django import forms
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if "password" in self.fields:
+            # 비밀번호 변경 문구 추가
+            self.fields["password"].help_text = (
+                '<a href="{}">여기</a>에서 비밀번호를 변경할 수 있어요.'.format(
+                    reverse("accounts:change_password")
+                )
+            )
+            # CSS를 활용해 필드 자체는 숨기지만 help_text는 남김
+            self.fields["password"].widget.attrs["style"] = "display: none;"
+            self.fields["password"].label = "비밀번호 변경"
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,87 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    PasswordChangeForm,
+)
+from django.contrib.auth import (
+    login as auth_login,
+    logout as auth_logout,
+    update_session_auth_hash,
+)
 from django.views.decorators.http import require_POST, require_http_methods
 from django.contrib.auth.decorators import login_required
+from .forms import CustomUserChangeForm
 
-        
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)  # 세션 유지
+            return redirect("articles:index2")
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {"form": form}
+    return render(request, "accounts/change_password.html", context)
+
+
+# @login_required
+# @require_http_methods(["GET", "POST"])
+# def change_password(request):
+#     if request.method == "POST":
+#         form = PasswordChangeForm(request.user, request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("articles.index2")
+#     else:
+#         form = PasswordChangeForm(request.user)
+#     context = {"form": form}
+#     return render(request, "accounts/change_password.html", context)
+
+
+@require_http_methods(["GET", "POST"])
+def update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            return redirect("articles:index2")
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {"form": form}
+    return render(request, "accounts/update.html", context)
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect("articles:index2")
+    else:
+        form = UserCreationForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
+
+
+@require_POST
+def delete(request):
+    if request.user.is_authenticated:
+        request.user.delete()
+        auth_logout(request)  # 탈퇴 후 해당 유저의 세션 지우기
+    return redirect("articles:index2")
+
+
 @require_POST
 def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
     return redirect("articles:index2")
+
 
 # def logout(request):
 #     if request.method=="POST":
@@ -17,16 +89,11 @@ def logout(request):
 #     return redirect("articles:index2")
 
 
-
-
-
-
-
-
 # def login(request):
 #     form = AuthenticationForm()
 #     context = {"form":form}
 #     return render(request, "accounts/login.html", context)
+
 
 def login(request):
     if request.method == "POST":
@@ -37,8 +104,5 @@ def login(request):
             return redirect("articles:index2")
     else:
         form = AuthenticationForm()
-    context = {"form":form}
+    context = {"form": form}
     return render(request, "accounts/login.html", context)
-
-
-
